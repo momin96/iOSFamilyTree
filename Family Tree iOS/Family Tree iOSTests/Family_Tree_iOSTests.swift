38b2e67fcd5832c3f632f9fd8386fb3bf60c40cd
Nasir <momin96@gmail.com>
2018-05-07 15:43:08 +0530

    Commented unused test script

--- Family Tree iOS/Family Tree iOSTests/Family_Tree_iOSTests.swift
+++ Family Tree iOS/Family Tree iOSTests/Family_Tree_iOSTests.swift
@@ -17,28 +17,20 @@ class Family_Tree_iOSTests: XCTestCase {
     }
     
     
-    func testDataDownload() {
-        NSRDataFetcher.shared.getRequestData { (data, response, err) in
-            XCTAssertNil(data != nil, "Data is nil")
-            XCTAssertNotNil(data != nil, "Not nil")
-        }
-    }
+//    func testDataDownload() {
+//        NSRDataFetcher.shared.getRequestData { (data, response, err) in
+//
+//            let httpResponse = response as! HTTPURLResponse
+//            XCTAssertEqual(httpResponse.statusCode == 200, err == nil)
+//
+//            XCTAssertNil(data == nil, "Data is nil")
+//            XCTAssertNotNil(data != nil, "Not nil")
+//        }
+//    }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
