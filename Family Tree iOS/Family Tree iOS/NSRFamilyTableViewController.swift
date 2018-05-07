62529d2140ef81e9fdf4434f9ce74574940dbc52
Nasir <momin96@gmail.com>
2018-05-07 11:12:24 +0530

    Data displaying on UI

--- Family Tree iOS/Family Tree iOS/NSRFamilyTableViewController.swift
+++ Family Tree iOS/Family Tree iOS/NSRFamilyTableViewController.swift
@@ -10,26 +10,56 @@ import UIKit
 
 class NSRFamilyTableViewController: UITableViewController {
 
+
+    @IBOutlet weak var familyTableView: UITableView!
+    
+    private var family: Family?
+    
+    // MARK: View life Cycle
+    deinit {
+        print("deinit of NSRFamilyTableViewController")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        NSRDataConstructor.constructFamilyData { (family) in
+            self.family = family
+            OperationQueue.main.addOperation({
+                self.familyTableView.reloadData()
+            })
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        print("didReceiveMemoryWarning in NSRFamilyTableViewController")
     }
     
+    
+    // MARK: TableView DataSource Methods
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return (family?.children?.count)!
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return nil
+        
+        let cell = UITableViewCell.init(style: .value1, reuseIdentifier: nil)
+
+        if let children = family?.children {
+            let child = children[indexPath.row]
+            cell.textLabel?.text = "Name: "+(String(describing: child.name))
+            cell.detailTextLabel?.text = "Age: "+String.init(describing: child.age)
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return family?.name
     }
  }
